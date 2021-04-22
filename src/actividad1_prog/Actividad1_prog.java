fe4b6e289c2863cbb3bca3bd45cfa607462dd6eb
Rafael Moreno <Rafael Moreno@LAPTOP-ROGBR6G0>
2021-04-21 23:23:22 -0500

    1

---
+++ src/actividad1_prog/Actividad1_prog.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad1_prog;
+       
+
+public class Actividad1_prog {
+    
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
